4cf0e102088bf4c25981b1e7dd5950dc9fd944f3
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-28 19:31:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == "" || " " || nil
+  if board[index] == "" || " " 
     return false
-  elsif board[index] == "X" || "O"
-    return true
+
 end
